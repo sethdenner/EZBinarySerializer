2fbbc626847b2d7cff20be121dde2f775f35beee
Seth Denner <sethden@gmail.com>
2026-04-06 19:09:28 -0700

    Adds Fixes and Updates Nuget Version
    * Moves test SampleModel to the Tests directory.
    * Fixes NugetIntegrationTests to use the new sample model correctly.
    * Updates Nuget version to 0.1.1-beta.

--- build-nuget.ps1
+++ build-nuget.ps1
@@ -1,5 +1,5 @@
 # Build the NuGet package
-dotnet pack -c Release -o ./artifacts -p:Version=0.1.0-beta -p:Authors='Seth Adam Denner' -p:Description='Easily serialize and deserialize instances using a simple attribute based interface. Supports polymorphism and many standard types including System.Collections.Generic and System.Numerics types. Uses zero reflection maintaining AOT compatibility for your game development embeded systems and other applications where performance and portability is desired.'
+dotnet pack ./EZBinarySerializer -c Release -o ./artifacts -p:Version=0.1.1-beta -p:Authors='Seth Adam Denner' -p:Description='Easily serialize and deserialize instances using a simple attribute based interface. Supports polymorphism and many standard types including System.Collections.Generic and System.Numerics types. Uses zero reflection maintaining AOT compatibility for your game development embeded systems and other applications where performance and portability is desired.'
 # Restore the project using the custom config file, restoring packages to a local folder
 dotnet restore ./Tests/NugetIntegrationTests --packages ./packages --configfile "./Tests/NugetIntegrationTests/nuget.integration-tests.config" 
 # Build the project (no restore), using the packages restored to the local folder
